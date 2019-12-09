911cb0eac976557270ccb23386fdffded175bcc2
Maxime <maxou3101@gmail.com>
2019-12-09 22:14:28 +0100

    test navbar == failed

--- Application/app/src/main/java/com/example/computer_horizon/MainActivity.java
+++ Application/app/src/main/java/com/example/computer_horizon/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.computer_horizon;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
