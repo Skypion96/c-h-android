3d14bd35e94a4fc6bccf467c27686dbf2d565717
Vincent <leurart.vincent@outlook.be>
2019-12-14 19:39:48 +0100

    création d'un nouveau panier en même temps que l'utilisateur fait

--- Application/app/src/main/java/com/example/computer_horizon/MainActivity.java
+++ Application/app/src/main/java/com/example/computer_horizon/MainActivity.java
@@ -106,8 +106,8 @@ public class MainActivity extends AppCompatActivity {
             panier.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this, Panier.class);
-                    startActivity(intent);
+                    /*Intent intent = new Intent(MainActivity.this, Panier.class);
+                    startActivity(intent);*/
                 }
             });
         }
